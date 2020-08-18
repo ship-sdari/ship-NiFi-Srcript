0cf55b4fefa56099c1d139f275ab3783731004b7
‘wlioe’ <528129061@qq.com>
2020-08-18 19:35:13 +0800

    wanghuaizhi : 分脚本 修改

--- nifi-script/ship-nifi-srcipt/src/main/groovy/com/sdari/processor/analysisDataBySid/list2mysqlJson.groovy
+++ nifi-script/ship-nifi-srcipt/src/main/groovy/com/sdari/processor/analysisDataBySid/list2mysqlJson.groovy
@@ -1,17 +1,29 @@
 package com.sdari.processor.analysisDataBySid
+
+import com.alibaba.fastjson.JSON
+import com.alibaba.fastjson.JSONArray
+
+//import com.sdari.dto.manager.TStreamRuleDTO
+
 //import com.alibaba.fastjson.JSONArray
 //import com.sdari.dto.manager.TStreamRuleDTO
 
 public class list2mysqlJson {
 
 
-    public static Object calculation(Map<String, Object> param) {
+    public static def calculation(def param) {
         if (null == param) return null
-        //  JSONArray dataList = param.get("data") as JSONArray
-        Map<String, String> attributes = param.get("attributes") as Map<String, String>
-        attributes.put("test1", "ok")
-        //  Map<String, Map<String, TStreamRuleDTO>> rules = param.get("rules") as Map<String, Map<String, TStreamRuleDTO>>
-        param.put("attributes", attributes)
+        Map<String, String> attributes = (param as LinkedHashMap).get("attributes") as Map<String, String>
+        String sid = attributes.get("sid")
+        JSONArray dataList = (param as LinkedHashMap).get("data") as JSONArray
+        JSONArray returnList = new ArrayList<>()
+        //    Map<String, Map<String, TStreamRuleDTO>> rules = (param as LinkedHashMap).get("rules") as Map<String, Map<String, TStreamRuleDTO>>
+        for (data in dataList) {
+            JSONArray ja = new JSONArray()
+            ja.add(data as JSON)
+            returnList.add(ja as JSONArray)
+        }
+        (param as LinkedHashMap).put("data", returnList)
         return param
     }
 
