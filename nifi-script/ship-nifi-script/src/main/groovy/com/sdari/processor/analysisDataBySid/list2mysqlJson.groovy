0cfc1f32ceb929f95ca346280ebfe06796445fe7
‘wlioe’ <528129061@qq.com>
2020-08-19 11:38:24 +0800

    wanghuaizhi : 分脚本 添加colTime 属性

--- nifi-script/ship-nifi-script/src/main/groovy/com/sdari/processor/analysisDataBySid/list2mysqlJson.groovy
+++ nifi-script/ship-nifi-script/src/main/groovy/com/sdari/processor/analysisDataBySid/list2mysqlJson.groovy
@@ -1,30 +1,33 @@
 package com.sdari.processor.analysisDataBySid
 
-import com.alibaba.fastjson.JSON
 import com.alibaba.fastjson.JSONArray
+import com.alibaba.fastjson.JSONObject
 
-//import com.sdari.dto.manager.TStreamRuleDTO
-
-//import com.alibaba.fastjson.JSONArray
-//import com.sdari.dto.manager.TStreamRuleDTO
+import java.time.Instant
 
 public class list2mysqlJson {
 
-
     public static def calculation(def param) {
         if (null == param) return null
-        Map<String, String> attributes = (param as LinkedHashMap).get("attributes") as Map<String, String>
-        String sid = attributes.get("sid")
-        JSONArray dataList = (param as LinkedHashMap).get("data") as JSONArray
-        JSONArray returnList = new ArrayList<>()
-        //    Map<String, Map<String, TStreamRuleDTO>> rules = (param as LinkedHashMap).get("rules") as Map<String, Map<String, TStreamRuleDTO>>
-        for (data in dataList) {
-            JSONArray ja = new JSONArray()
-            ja.add(data as JSON)
-            returnList.add(ja as JSONArray)
+        List<LinkedHashMap> returnList = new ArrayList<>()
+        for (paramMap in (param as List<LinkedHashMap>)) {
+            //获取流文件属性
+            Map<String, String> attributes = paramMap.get("attributes") as Map<String, String>
+            def rules = paramMap.get("rules")
+            //获取数据
+            def dataList = paramMap.get("data")
+            for (data in (dataList as JSONArray)) {
+                LinkedHashMap map = new LinkedHashMap()
+                def dataAttribute = attributes
+                JSONObject jsonData = data as JSONObject
+                dataAttribute.put("coltime", String.valueOf(Instant.ofEpochMilli(jsonData.getLongValue("time"))))
+                jsonData.remove("time")
+                map.put("rules", rules)//规则
+                map.put("attributes", dataAttribute)//属性
+                map.put("data", jsonData)
+                returnList.add(map)
+            }
         }
-        (param as LinkedHashMap).put("data", returnList)
-        return param
+        return returnList
     }
-
 }
