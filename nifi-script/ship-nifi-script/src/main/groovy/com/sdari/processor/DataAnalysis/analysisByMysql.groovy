04139b295f7e038c693704150dfc727876e4a31a
‘wlioe’ <528129061@qq.com>
2020-09-07 11:22:05 +0800

    wanghuaizhi :修改参数格式

--- nifi-script/ship-nifi-script/src/main/groovy/com/sdari/processor/DataAnalysis/analysisByMysql.groovy
+++ nifi-script/ship-nifi-script/src/main/groovy/com/sdari/processor/DataAnalysis/analysisByMysql.groovy
@@ -38,7 +38,7 @@ class analysisByMysql {
     final static String OPTION = 'option'
     final static String META = 'meta'
     //mysql 处理使用参数
-    final static String tables = 'es_tables'
+    final static String tables = 'es.tables'
     final static String optionSTATUS = "optionSTATUS"
     //时间相关参数
     final static String time_type = "yyyy-MM-dd HH:mm:ss"
