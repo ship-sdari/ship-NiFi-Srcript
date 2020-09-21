99e4f12e6f47b3ff46eb8a886e7a4a5e79d26819
WF-Liumouren <3553108225@qq.com>
2020-09-21 17:55:31 +0800

    Liumouren : KPI计算指标（主机用油：host_use_oil）

---
+++ nifi-script/ship-nifi-script/src/main/groovy/com/sdari/processor/CalculationKPI/HostUseOilDTO.groovy
@@ -0,0 +1,110 @@
+package com.sdari.processor.CalculationKPI
+
+
+import com.alibaba.fastjson.JSONObject
+
+import java.time.Instant
+
+/**
+ *
+ * @type: （单机单桨）
+ * @kpiName: 主机用油
+ * @author Liumouren
+ * @date 2020-09-21 17:51:00
+ */
+class HostUseOilDTO {
+    private static log
+    private static processorId
+    private static String processorName
+    private static routeId
+    private static String currentClassName
+
+    //指标名称
+    private static kpiName = 'host_use_oil'
+    //计算相关参数
+    final static String SID = 'sid'
+
+    HostUseOilDTO(final def logger, final int pid, final String pName, final int rid) {
+        log = logger
+        processorId = pid
+        processorName = pName
+        routeId = rid
+        currentClassName = this.class.canonicalName
+        log.info "[Processor_id = ${processorId} Processor_name = ${processorName} Route_id = ${routeId} Sub_class = ${currentClassName}] 初始化成功！"
+    }
+
+    static def calculation(params) {
+        if (null == params) return null
+        def returnMap = [:]
+        def dataListReturn = []
+        def attributesListReturn = []
+        final List<JSONObject> dataList = (params as HashMap).get('data') as ArrayList
+        final List<JSONObject> attributesList = ((params as HashMap).get('attributes') as ArrayList)
+        final Map<String, Map<String, JSONObject>> rules = ((params as HashMap).get('rules') as Map<String, Map<String, JSONObject>>)
+        final Map processorConf = ((params as HashMap).get('parameters') as HashMap)
+        final Map shipConf = ((params as HashMap).get('shipConf') as HashMap)
+        //循环list中的每一条数据
+        for (int i = 0; i < dataList.size(); i++) {
+            JSONObject json = new JSONObject()
+            final JSONObject JsonData = (dataList.get(i) as JSONObject)
+            final JSONObject jsonAttributesFormer = (attributesList.get(i) as JSONObject)
+
+            String sid = jsonAttributesFormer.get(SID)
+            String coltime = String.valueOf(Instant.now())
+            //  String coltime = jsonAttributesFormer.get(COLTIME)
+            //判断数据里是否 有 当前计算指标数据
+            if (!JsonData.containsKey(kpiName)) {
+                log.debug("[${sid}] [${kpiName}] [没有当前指标 计算所需的数据] result[${null}] ")
+                json.put(kpiName, null)
+            } else {
+                Map<String, BigDecimal> maps = JsonData.get(kpiName) as Map<String, BigDecimal>
+                BigDecimal result = calculationKpi((shipConf.get(sid) as Map<String, String>), maps, coltime, sid)
+                json.put(kpiName, result)
+            }
+            //单条数据处理结束，放入返回
+            dataListReturn.add(json)
+            attributesListReturn.add(jsonAttributesFormer)
+        }
+        //全部数据处理完毕，放入返回数据后返回
+        returnMap.put('rules', rules)
+        returnMap.put('shipConf', shipConf)
+        returnMap.put('data', dataListReturn)
+        returnMap.put('parameters', processorConf)
+        returnMap.put('attributes', attributesListReturn)
+        return returnMap
+    }
+
+    /**
+     * 主机用油的计算公式
+     * 计算公式为无
+     *
+     * @param configMap 相关系统配置
+     * @param data 参与计算的信号值<innerKey,value></>
+     */
+    static BigDecimal calculationKpi(Map<String, String> configMap, Map<String, BigDecimal> data, final String time, final String sid) {
+        try {
+            BigDecimal result = null;
+            // 主机使用重油指示
+            BigDecimal meUseHfoStr = data.get("me_use_hfo");
+            //主机使用柴油/轻柴油指示
+            BigDecimal meUseMdoStr = data.get("me_use_mdo");
+            if(null==meUseHfoStr&&null==meUseMdoStr){
+                log.debug("[${sid}] [${kpiName}] [${time}] 主机使用重油指示[${meUseHfoStr}] 主机使用柴油/轻柴油指示[${meUseMdoStr}] result[${null}] ")
+                return null;
+            }
+            //计算
+            if (null!=meUseHfoStr&&meUseHfoStr.compareTo(BigDecimal.ONE) == 0) {
+                result = BigDecimal.valueOf(0);
+            }else if (null!=meUseMdoStr&&meUseMdoStr.compareTo(BigDecimal.ONE) == 0){
+                result = BigDecimal.valueOf(1);
+            }else if(null!=meUseMdoStr&&meUseMdoStr.compareTo(BigDecimal.ZERO) == 0){
+                result = BigDecimal.valueOf(0);
+            }
+            log.debug("[${sid}] [${kpiName}] [${time}] 主机使用重油指示[${meUseHfoStr}] 主机使用柴油/轻柴油指示[${meUseMdoStr}] result[${result}] ")
+            return result
+        } catch (Exception e) {
+            log.error("[${sid}] [${kpiName}] [${time}] 计算错误异常:${e} ")
+            return null
+        }
+    }
+}
